47835037aeb3ed7131fd9fa744a4b06924e6cb53
umlautonmuller <fernando@muellers.com.br>
2021-11-11 23:37:23 -0300

    Added generation, redirection and error catching

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
 import express from "express"
+import random from "randomstring"
 
 const app = express()
 
-app.get("/", (request, response) => response.send("Hello world"))
+app.use(express.json())
+
+const urls = {}
+
+app.post("/generate", (request, response) => {
+    const source = request.body.url
+
+    const destination = random.generate({
+        length: 9,
+        charset: 'alphabetic'
+    })
+
+    urls[destination] = source
+
+    response.json({
+        url: `http://${request.headers.host}/${destination}`
+    })
+})
+
+app.get("/404", (request, response) => {
+    response.status(404).send('Você errou').end()
+})
+
+app.get("/:url", (request, response) => {
+    const url = urls[request.params.url]
+
+    if (url === undefined) {
+        response.redirect('/404')
+    } else {
+        response.redirect(url)
+    }
+})
 
 app.listen(8000, () => console.log("Funfou"))
